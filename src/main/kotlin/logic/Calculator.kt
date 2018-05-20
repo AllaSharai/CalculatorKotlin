dff9ff9ccfda600fcf26f7a27029051c33f90f0f
Alla Sharai <alla.sharai19@gmail.com>
2018-05-20 21:36:37 +0200

    Add functionality without brackets

--- src/main/kotlin/logic/Calculator.kt
+++ src/main/kotlin/logic/Calculator.kt
@@ -6,20 +6,19 @@ class Calculator {
 
     companion object {
 
-        var numbersStack: Stack<Double> = Stack()
-        var operatorsStack: Stack<Char> = Stack()
+        private var numbersStack: Stack<Double> = Stack()
+        private var operatorsStack: Stack<Char> = Stack()
 
-        fun calculate(expression: String) : Double{
+        fun calculate(expression: String): Double {
             numbersStack.clear()
             operatorsStack.clear()
-            var result = 0.0
-            splitExpression(expression.replace("(-", "(0-"))
-            return result
+            return calculateExpression(expression.replace("(-", "(0-"))
         }
 
-        private fun splitExpression(expression: String) {
-            var number: String = String()
+        private fun calculateExpression(expression: String): Double {
+            var number = String()
             for (symbol in expression) {
+
                 if (symbol.isDigit() || symbol == '.') {
                     number = number.plus(symbol)
 
@@ -37,10 +36,42 @@ class Calculator {
             }
             println(numbersStack)
             println(operatorsStack)
+
+            while (!operatorsStack.empty()){
+                popOperator()
+            }
+            return numbersStack.pop()
         }
 
         private fun operator(operator: Char) {
+            if (canPop(operator)) {
+                popOperator()
+            }
             operatorsStack.push(operator)
+        }
+
+        private fun popOperator() {
+            val B = numbersStack.pop()
+            val A = numbersStack.pop()
+
+            when (operatorsStack.pop()) {
+                '+' -> numbersStack.push(A + B)
+                '-' -> numbersStack.push(A - B)
+                '*' -> numbersStack.push(A * B)
+                '/' -> numbersStack.push(A / B)
+            }
+        }
+
+        private fun canPop(operator: Char): Boolean {
+            return !operatorsStack.empty() && getPriority(operator) <= getPriority(operatorsStack.peek())
+        }
+
+        private fun getPriority(operator: Char): Int {
+            when (operator) {
+                '+', '-' -> return 1
+                '*', '/' -> return 2
+            }
+            return -1
         }
 
     }
